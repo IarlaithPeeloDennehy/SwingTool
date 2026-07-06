3fb0ebe4b7c0b2e2d0a7ba9d369f19dc7b66354f
Iarlaith Peelo Dennehy <Iarlaith.peelodennehy.2021@mumail.ie>
2026-07-06 22:18:33 +0100

    pos change

--- swingtool/__init__.py
+++ swingtool/__init__.py
@@ -1,3 +1,3 @@
-"""SwingTool: computer-vision analysis of golf swings from a single phone video."""
+"""SwingTool: computer-vision analysis of golf swings from a single (Landscape) phone video."""
 
 __version__ = "0.1.0"
